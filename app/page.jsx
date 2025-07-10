6ac43bd3f84849cc86b57da8dbf9fc56387f6298
Ashish Saxena <ashish.sxn847@gmail.com>
2025-07-10 16:34:14 +0530

    home page image done

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
+import HeroSection from "@/components/Hero";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
         <div className="grid-background"></div>
+
+        <HeroSection />
     </div>
   );
 }
---
+++ components/Hero.jsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from 'next/image';
+
+const HeroSection = () => {
+  return (
+    <section className='w-full pt-36 md:pt-48 pb-10'>
+        <div className='space-y-6 text-center'>
+            <div className='space-y-6 mx-auto'>
+                <h1 className='gradient-title text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold'>
+                    Your AI Career Coach for
+
+                    <br />
+                    Professional Success
+                </h1>
+                <p className='mx-auto max-w-[600px] text-muted-foreground text-lg md:text-xl '>
+                    Advanced your career with personalized guidance, interview prep , and
+                    AI-powered tools for job success. 
+                </p>
+            </div>
+            <div>
+                <Link href="/dashboard">
+                    <Button size="lg" className='px-8'>
+                        Get Started
+                    </Button>
+                </Link>
+            </div>
+            <div className='hero-image-wrapper mt-5 md:mt-0'>
+                <div>
+                    <Image src={"/banner.jpeg"} width={1280} height={720} alt='Banner Sensai' className='rounded-lg  shadow-2xl border mx-auto' 
+                        priority
+                    />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
