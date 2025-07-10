8d5e4d5695e002519dd002e18807491fd5912a53
Ashish Saxena <ashish.sxn847@gmail.com>
2025-07-10 18:04:09 +0530

    features added

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,49 @@
 import HeroSection from "@/components/Hero";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { features } from "@/data/features";
 
 export default function Home() {
   return (
     <div>
-        <div className="grid-background"></div>
+      <div className="grid-background"></div>
+      <HeroSection />
 
-        <HeroSection />
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+        <div className="container mx-auto px-4 md:px-6">
+          <h2 className="text-3xl font-bold tracking-tighter text-center mb-12 ">
+            Powerful features for Your Career Growth
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+            {features?.map((feature, idx) => {
+              return (
+                <Card
+                  key={idx}
+                  className="border-2 hover:border-primary transition-colors duration-300 cursor-pointer"
+                >
+                  <CardContent className="pt-6 text-center flex flex-col items-center">
+                    <div className="flex flex-col items-center justify-center">
+                      {feature?.icon}
+                      <h3 className="text-xl font-bold mb-2">
+                        {feature?.title}
+                      </h3>
+                      <p className="text-muted-foreground">
+                        {feature?.description}
+                      </p>
+                    </div>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-muted">
+        <div className="container mx-auto px-4 md:px-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto"></div>
+        </div>
+      </section>
     </div>
   );
 }
